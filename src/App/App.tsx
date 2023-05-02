10870580d115ce023aadd23736fb43b464e5875e
jonathanboris <ouedraogojonathanboris@gmail.com>
2023-05-02 16:30:11 +0200

    changement de style, prototype, props

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import Button from './components/ui/Button/Button';
 
 function App() {
   return (
     <div className="App" data-testid="App">
       DEMAT
+      <Button backgroundColor='blue' onClick={()=>{console.log('hello')}}>
+
+      </Button>
     </div>
   );
 }
---
+++ src/App/components/ui/Button/Button.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+import styles from "./Button.module.css";
+import propTypes from "prop-types";
+
+const Button = (props) => {
+
+    console.log(props)
+    return (
+        
+<button  style={{backgroundColor:props.backgroundColor}} className={styles.Button} onClick={(evt)=>{
+    console.log(evt) 
+    props.onClick('le roi albert')}}>
+            {props.children}
+        </button>
+    )
+}
+
+Button.propTypes = {
+    onClick: propTypes.func.isRequired,
+    children: propTypes.any.isRequired,
+    backgroundColor: propTypes.string
+}
+
+Button.defaultProps = {
+    children: 'click me',
+    onClick: ()=>{},
+    backgroundColor: 'blue'
+}
+
+export default Button;
